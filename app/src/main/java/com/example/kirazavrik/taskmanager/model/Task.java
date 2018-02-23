99e7ee5c2b0cdf023cbcbda2f429fe7ac8b58f42
kirazavrik <wr30mg@gmail.com>
2018-02-23 16:17:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kirazavrik/taskmanager/model/Task.java
@@ -0,0 +1,18 @@
+package com.example.kirazavrik.taskmanager.model;
+
+/**
+ * Created by kirazavrik on 23.2.18.
+ */
+
+public class Task {
+
+    private String taskTest;
+
+    public String getTaskTest() {
+        return taskTest;
+    }
+
+    public void setTaskTest(String taskTest) {
+        this.taskTest = taskTest;
+    }
+}
